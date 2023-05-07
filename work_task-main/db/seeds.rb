419d7c948c7f5879cbbbb90711f5bbe6bbefd70f
burittossss <denys.m.bura@ukd.edu.ua>
2023-05-08 01:36:08 +0300

    first commit

---
+++ work_task-main/db/seeds.rb
@@ -0,0 +1,28 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+# movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Movie.create(
+  [
+    { name: "The maze runner 1", year: 2014 },
+    { name: "Lord of the Rings", year: 2002 },
+    { name: 'Avengers 3', year: 2018 },
+    { name: 'The Matrix', year: 1999 },
+    { name: 'Spider-man 3', year: 2007 },
+    { name: 'The Dark Knight', year: 2008 },
+    { name: 'The Godfather', year: 1972 },
+    { name: 'Escape from Shoushenk', year: 1994 },
+    { name: 'Hacksaw ridge', year: 2016 },
+    { name: 'Pulp Fiction', year: 1994 },
+    { name: 'The Good, the Bad and the Ugly', year: 1966 },
+    { name: 'The Lord of the Rings: The Fellowship of the Ring', year: 2001 },
+    { name: 'The Locksmith', year: 2023 },
+    { name: 'Venom', year: 2018 },
+    { name: 'Fight Club', year: 1999 },
+    { name: 'Black panther', year: 2022 },
+  ]
+)
